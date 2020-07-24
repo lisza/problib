d44adf5a18e474f0e619c45c4ccd4c94d94eeb9a
lisza <liszalisza@gmail.com>
2020-07-24 15:09:01 -0700

    Add license, adjsut setup

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(name='problib',
       version='0.2',
       description='Gaussian and Binomial Probability Distributions',
+      license='MIT',
+      author='lisza',
       packages=['problib'],
       zip_safe=False)
