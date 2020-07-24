a3b301225094313d6b826be9ac083c438b813c27
lisza <liszalisza@gmail.com>
2020-07-24 12:56:55 -0700

    Add basic setup file for package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='problib',
+      version='0.2',
+      description='Gaussian and Binomial Probability Distributions',
+      packages=['problib'],
+      zip_safe=False)
